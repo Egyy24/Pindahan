4ba8cf0a614ba686ce49339fcad1e7bb20ec3ecd
Qanzul Arays <qanzularays@gmail.com>
2026-06-29 21:34:26 +0700

    Redirect ke pindahan.html

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Pindahan - Asisten Pindahan',
+  description: 'Aplikasi asisten pindahan rumah',
+  generator: 'next.js',
   icons: {
     icon: [
       {
